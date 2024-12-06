e921349326cf118e3e9875718e2220ef47733826
MateBar <barbirm@mail.com>
2024-12-06 19:37:27 +0100

    Add randomized messages section

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ const milk = ['regular full fat/skinny', 'soy', 'almond', 'oat', 'coconut', 'pea
 const flavours_index = Math.floor(Math.random() * flavours.length)
 const add_ons_index = Math.floor(Math.random() * add_ons.length)
 const milk_index = Math.floor(Math.random() * milk.length)
+
+//Randomized message
+console.log('Welcome to our "Chilled Delights" smoothie shop!')
+console.log('You\'re craving a delightful smoothie, but you can\'t make up your mind what flavours to choose?')
+console.log('Don\'t worry, our "Smoothie Generator" is here to help!')
+console.log(`You should have ${flavours[flavours_index]} and ${add_ons[add_ons_index]} smoothie with ${milk[milk_index]} milk.`)
+console.log('Enjoy!')
