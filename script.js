8088865720cdde91789f2d3977cf757ae5268eeb
MateBar <barbirm@mail.com>
2024-12-06 18:49:06 +0100

    Add third array with milk items

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 // Arrays with items for randomly generated message
 const flavours = ['strawberry', 'raspberry', 'blueberry', 'blackberry', 'mango', 'papaya', 'pineapple', 'avocado', 'peach', 'apricot', 'banana', 'green apple']
 const add_ons = ['cocoa nibs', 'coconut shavings', 'peanut butter', 'chia seeds', 'matcha', 'macadamia nuts', 'madagascar vanilla', 'pistachio', 'spirulina', 'acacia honey', 'fresh ginger', 'medjool dates']
+const milk = ['regular full fat/skinny', 'soy', 'almond', 'oat', 'coconut', 'pea', 'hazelnut', 'cashew', 'potato', 'hemp', 'rice', 'tiger nut']
