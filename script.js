81b66b1417d0379215725ee2579f9879219359f7
MateBar <barbirm@mail.com>
2024-12-06 18:17:22 +0100

    Add the first array of items

---
+++ script.js
@@ -0,0 +1,2 @@
+// Arrays with items for randomly generated message
+const flavours = ['strawberry', 'raspberry', 'blueberry', 'blackberry', 'mango', 'papaya', 'pineapple', 'avocado', 'peach', 'apricot', 'banana', 'green apple']
