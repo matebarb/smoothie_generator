084e93b51fbdc43027e15c959bcc9b9874b5cfa4
MateBar <barbirm@mail.com>
2024-12-06 18:42:26 +0100

    Add second, addons array

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 // Arrays with items for randomly generated message
 const flavours = ['strawberry', 'raspberry', 'blueberry', 'blackberry', 'mango', 'papaya', 'pineapple', 'avocado', 'peach', 'apricot', 'banana', 'green apple']
+const add_ons = ['cocoa nibs', 'coconut shavings', 'peanut butter', 'chia seeds', 'matcha', 'macadamia nuts', 'madagascar vanilla', 'pistachio', 'spirulina', 'acacia honey', 'fresh ginger', 'medjool dates']
